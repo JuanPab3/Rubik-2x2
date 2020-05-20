5432d35063f8063a12784593cfa69cb5ba940cf6
Juan P. Sierra <juanpab.sierra@urosario.edu.co>
2020-05-20 09:51:12 -0500

    Se fue con toda
    issue no: UD044

--- Main/main.py
+++ Main/main.py
@@ -839,9 +839,9 @@ def regla17():
 
 #====================================CODIGO=====================================
 
-reglas = [regla0(),regla1(),regla2(),regla3(),regla4(),regla5(),regla6(),regla7(),regla8(),regla9(),regla10(),regla11(),regla12(),regla13(),regla14(),regla15(),regla16(Nturnos),regla17()]
+reglas = [basic_0(),regla1(),regla2(),regla3(),regla4(),regla5(),regla6(),regla7(),regla8(),regla9(),regla10(),regla11(),regla12(),regla13(),regla14(),regla15(),regla16(Nturnos),regla17()]
 #
-# formula  = regla15()
+# formula  = basic_0()
 # fFNC = fn.Tseitin(formula, lC)
 
 formula_de_la_muere  = "((((((((((((((((({0}Y{1})Y{2})Y{3})Y{4})Y{5})Y{6})Y{7})Y{8})Y{9})Y{10})Y{11})Y{12})Y{13})Y{14})Y{15})Y{16})Y{17})".format(reglas[0],reglas[1],reglas[2],reglas[3],reglas[4],reglas[5],reglas[6],reglas[7],reglas[8],reglas[9],reglas[10],reglas[11],reglas[12],reglas[13],reglas[14],reglas[15],reglas[16],reglas[17])
@@ -864,7 +864,7 @@ test = {}
 print("inicio a DPLL")
 test = dpll(fClaus,test)
 
-#sat, dic = test
+sat, dic = test
 
 print(test)
 
@@ -879,19 +879,17 @@ with open("satisfacible.txt", "w") as f:
     for i in final:
         f.write("{} {} {} {}\n".format(i[0],i[1],i[2],i[3]))
 
-file_list = []
-with open("satisfacible.txt","r") as f:
-    for line in f:
-        file_list.extend([line.split()])
-
-for l in file_list:
-    # l[0] = "T{}".format(l[0])
-    # l[1] = "S{}".format(l[1])
-    # l[2] = "C{}".format(l[2])
-    l[0] = "{}".format(l[0])
-    l[1] = "{}".format(l[1])
-    l[2] = "{}".format(l[2])
-
-colores = lis_to_col(file_list,Nturnos,Ncuadros,Ncolores)
-
-print(file_list)
+# file_list = []
+# with open("satisfacible.txt","r") as f:
+#     for line in f:
+#         file_list.extend([line.split()])
+#
+# for l in file_list:
+#     # l[0] = "T{}".format(l[0])
+#     # l[1] = "S{}".format(l[1])
+#     # l[2] = "C{}".format(l[2])
+#     l[0] = "{}".format(l[0])
+#     l[1] = "{}".format(l[1])
+#     l[2] = "{}".format(l[2])
+#
+# colores = lis_to_col(file_list,Nturnos,Ncuadros,Ncolores)
